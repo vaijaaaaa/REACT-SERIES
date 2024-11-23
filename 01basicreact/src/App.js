c4470e27af2409a73f84bfc79dba5162976b66f4
vaijaaaaa <vaijuwaker111@gmail.com>
2024-11-23 17:16:00 +0530

    next changes

--- 01basicreact/src/App.js
+++ 01basicreact/src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+
+import youtube from "./youtube";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+         <h1>classic react appp</h1>
+         <youtube/>
     </div>
+
   );
 }
 
---
+++ 01basicreact/src/youtube.jsx
@@ -0,0 +1,7 @@
+function youtube (){
+    return(
+        <h1> classic react app</h1>
+    )
+}
+
+export default youtube
